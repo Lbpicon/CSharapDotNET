fd0016f1d8ef59cb1580bbbf591ce07296862df1
lbpicon <lrn.picon@gmail.com>
2019-05-20 19:14:42 -0500

    Tic Tac Toe game

---
+++ CSharpPrograms/ConsoleApp1/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            /*introduction();*/
+            char player = 'X';
+            char[,] board = new char[3, 3];
+            bool gameOver = false;
+
+            // Intializing Board function
+            Intialize(board);
+
+           /* board[0, 0] = player;
+            player = 'O';
+            board[2, 2] = player;
+            player = 'X';*/
+
+            Print(board); // Loads board for user input
+
+            // Game plays until winner/loser/tie
+            while (gameOver == false) // Ask about this part
+            {
+                
+                              
+                Console.Write("Please enter row:");
+                int row = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter column:");
+                int col = Convert.ToInt32(Console.ReadLine());
+                
+
+                Console.WriteLine(" row: " + row + " col: " + col);
+
+                board[row, col] = player; // places X by player choice
+
+                if (player == 'X')
+                {
+
+                    if (player == board[0, 0] && player == board[0, 1] && player == board[0, 2]) // Across Board for X player
+                    {
+                        Console.WriteLine("Player X has won the game!");
+                        gameOver = true;
+                        Console.ReadKey();
+                        break;
+                    }
+
+                    if (player == board[0, 0] && player == board[1, 0] && player == board[2, 0])  // Down Board for X Player
+                    {
+                        Console.WriteLine("Player X has won the game!");
+                        Console.ReadKey();
+
+                    }
+
+                    if (player == board[0, 1] && player == board[1, 1] && player == board[2, 1]) // Middle Down
+                    {
+                        Console.WriteLine("Player X has won the game!");
+                        Console.ReadKey();
+
+                    }
+
+                    if (player == board[0, 2] && player == board[1, 2] && player == board[2, 2]) // Right Down
+                    {
+                        Console.WriteLine("Player X has won the game!");
+                        Console.ReadKey();
+
+                    }
+
+                    if (player == board[0, 2] && player == board[1, 1] && player == board[2, 2])
+                    {
+                        Console.WriteLine("Player X has won the game!");
+                        Console.ReadKey();
+                    }
+
+                    if (player == board[1, 0] && player == board[1, 1] && player == board[1, 2])
+                    {
+                        Console.WriteLine("Player X has won the game!");
+                        Console.ReadKey();
+
+                    }
+
+                }
+                
+
+            if (player == 'O')
+                {
+                    if (player == board[0, 0] && player == board[0, 1] && player == board[0, 2]) // Across Board for O player
+                    {
+                        Console.WriteLine("Player O has won the game!");
+                        Console.ReadKey();
+                        break;
+                    }
+
+                    if (player == board[0, 0] && player == board[1, 0] && player == board[2, 0])  // Down Board for O Player
+                    {
+                        Console.WriteLine("Player O has won the game!");
+                        Console.ReadKey();
+
+                    }
+
+                    if (player == board[0, 1] && player == board[1, 1] && player == board[2, 1]) // Middle Down
+                    {
+                        Console.WriteLine("Player O has won the game!");
+                        Console.ReadKey();
+
+                    }
+
+                    if (player == board[0, 2] && player == board[1, 2] && player == board[2, 2]) // Right Down
+                    {
+                        Console.WriteLine("Player O has won the game!");
+                        Console.ReadKey();
+
+                    }
+
+                    if (player == board[0, 2] && player == board[1, 1] && player == board[2, 2])
+                    {
+                        Console.WriteLine("Player O has won the game!");
+                        Console.ReadKey();
+                    }
+
+                    if (player == board[1, 0] && player == board[1, 1] && player == board[1, 2])
+                    {
+                        Console.WriteLine("Player O has won the game!");
+                        Console.ReadKey();
+                       }
+                  
+
+
+                    }
+
+
+             player = ChangeTurn(player); // changes to O
+
+                
+                Print(board); // calling method 
+                /*Console.Read(); */
+            }
+        }
+
+        static char ChangeTurn(char currentPlayer) //  change into function for call
+        {
+            
+            if (currentPlayer == 'X')
+            {
+                return 'O';
+            }
+            else
+            {
+                return 'X';
+            }
+        }
+
+
+
+
+       /* public static void introduction()
+        {
+            Console.Title = ("Tic Tac Toe, Yo");
+            Console.WriteLine("Welcome to Tic Tac Toe, Yo");
+            Console.WriteLine("Press any key to continue");
+        }*/
+
+        static void Intialize(char[,] board) // Intializing Board -> called into function
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+        }
+
+
+        //Printing Board
+         static void Print(char[,] board)
+        {
+            /*Console.Write(" 0 1 2 ");*/
+            for (int row = 0; row < 3; row++)
+            {
+                   
+                Console.Write(row + " | ");
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(board[row, col]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+
+               
+    }
+
+    
+}
+
+
+/*
+INPUT VALIDATION for ENTER NUMBER 
+if {
+int row =! 0 && int row != 1 && int row != 2 
+}
+*/
--- CSharpPrograms/tictactoe/Program.cs
+++ CSharpPrograms/tictactoe/Program.cs
@@ -9,9 +9,9 @@ namespace TicTacToe
         public static string playerTurn = "X";
         public static string[][] board = new string[][]
         {
-            new string[] {" ", " ", " "},
-            new string[] {" ", " ", " "},
-            new string[] {" ", " ", " "}
+            new string[] {"   ", "   ", "   "},
+            new string[] {"   ", "   ", "   "},
+            new string[] {"   ", "   ", "   "}
         };
 
         public static void Main()
@@ -26,22 +26,32 @@ namespace TicTacToe
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
-
+        #region
         public static void GetInput()
         {
             Console.WriteLine("Player " + playerTurn);
             Console.WriteLine("Enter Row:");
             int row = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Column:");
-            int column = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
         }
 
-        public static void PlaceMark(int row, int column)
+        public static void PlaceMark(int row, int col)
         {
-            
+           if playerTurn == 
 
         }
+         { 
+            
+        
+            
+            
+        }
 
+
+
+
+        }
         public static bool CheckForWin()
         {
             // your code goes here
@@ -76,18 +86,34 @@ namespace TicTacToe
 
             return false;
         }
-
+        #endregion
         public static void DrawBoard()
         {
             Console.WriteLine("  0 1 2");
-            Console.WriteLine("0 " + String.Join("|", board[0]));
-            Console.WriteLine("  -----");
-            Console.WriteLine("1 " + String.Join("|", board[1]));
-            Console.WriteLine("  -----");
-            Console.WriteLine("2 " + String.Join("|", board[2]));
+            Console.WriteLine("0 " + String.Join(" | ", board[0]));
+            Console.WriteLine("  ----------");
+            Console.WriteLine("1 " + String.Join(" | ", board[1]));
+            Console.WriteLine("  ----------");
+            Console.WriteLine("2 " + String.Join(" | ", board[2]));
+        }
+
+        public static void BoardLayout()
+        {
+            for (int j=0; j < 3; j++)
+            {
+                Console.WriteLine("|");
+            for (int k=0; k < 3; k++)
+                {
+                    Console.Write("| X ");
+                }
+            }
+        }
+        
+
+
         }
     }
-}
+
 
 /*for (int i=0; i<3; i++)
  { console.write("| ");
@@ -95,4 +121,4 @@ namespace TicTacToe
  Console.WriteLine("|");
  }
 
-   Console.WriteLine("-------");
+   Console.WriteLine("-------");*/
